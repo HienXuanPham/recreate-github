b6d00edd429218108b6f541e924d6f1712effbcd
Xuan Hien Pham <phamxuanhien02@gmail.com>
2023-01-24 21:25:46 -0500

    created component files

---
+++ src/components/LoginControls.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LoginControls = () => {
+  return (
+    <nav>
+      <a href="#Sign-in">Sign in</a>
+      <a href="#Sign-up">Sign up</a>
+    </nav>
+  );
+};
+
+export default LoginControls;
---
+++ src/components/NavigationBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./NavigationBar.css";
+
+const NavigationBar = () => {
+  return (
+    <nav className="navigation-bar">
+      <a href="#Why GitHub?">Why GitHub?</a>
+      <a href="#Team">Team</a>
+      <a href="#Enterprise">Enterprise</a>
+      <a href="#Explore">Explore</a>
+      <a href="#Marketplace">Marketplace</a>
+      <a href="#Pricing">Pricing</a>
+    </nav>
+  );
+};
+
+export default NavigationBar;
